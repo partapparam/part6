f408811439a3279714a531b4e033aa0015da2124
partapparam <paramsingh1961@gmail.com>
2022-08-13 19:52:53 -0700

    chagnes

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import logo from "./logo.svg"
 import "./App.css"
 import React from "react"
 import Note from "./Note"
+import { useDispatch, useSelector } from "react-redux"
+import { toggleImportanceOf } from "./reducers/noteReducers"
 
 function App({ store }) {
+  const dispatch = useDispatch()
+  const notes = useSelector((state) => state)
+  const toggleImportant = (id) => {
+    dispatch(toggleImportanceOf(id))
+  }
   // store.subscribe(() => {
   //   const stateNow = store.getState()
   //   console.log("this is the state now", stateNow)
@@ -17,7 +24,14 @@ function App({ store }) {
   // console.log(store.getState())
   return (
     <div className="App">
-      <Note store={store} />
+      <Note />
+      <ul>
+        {notes.map((note) => (
+          <li key={note.id} onClick={() => toggleImportant(note.id)}>
+            {note.content} <strong>{note.important ? "important" : ""}</strong>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
--- src/Note.jsx
+++ src/Note.jsx
@@ -2,9 +2,8 @@ import React from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { createNote, toggleImportanceOf } from "./reducers/noteReducers"
 
-const Note = ({ store }) => {
+const Note = (props) => {
   const dispatch = useDispatch()
-  const notes = useSelector((state) => state)
 
   const addNote = (event) => {
     event.preventDefault()
@@ -13,41 +12,12 @@ const Note = ({ store }) => {
     dispatch(createNote(content))
   }
 
-  const toggleImportant = (id) => {
-    dispatch(toggleImportanceOf(id))
-  }
-
-  store.dispatch({
-    type: "newNote",
-    data: {
-      content: "this is note",
-      important: true,
-      id: 1,
-    },
-  })
-
-  store.dispatch({
-    type: "newNote",
-    data: {
-      content: "state changes are made with actions",
-      important: false,
-      id: 2,
-    },
-  })
-
   return (
     <div className="App">
       <form onSubmit={addNote}>
         <input name="note" />
         <button type="submit">add</button>
       </form>
-      <ul>
-        {notes.map((note) => (
-          <li key={note.id} onClick={() => toggleImportant(note.id)}>
-            {note.content} <strong>{note.important ? "important" : ""}</strong>
-          </li>
-        ))}
-      </ul>
     </div>
   )
 }
